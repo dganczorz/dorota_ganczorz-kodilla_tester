3c157cd63489cf18a3b1b7ebf7ae58cf551dc354
Dorota Ganczorz <dganczorz@gmail.com>
2025-06-05 16:34:12 +0200

    Add MainExtended

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Dorota");
+        System.out.println(33);
+        System.out.println("G");
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String bookName = "Harry Potter";
+        int year = 1997;
+        boolean isRead = true;
+        String authorName = "Joanne Murray";
+        String authorLastName = "Rowling";
+        boolean isLiked = true;
+
+        System.out.println(bookName);
+        System.out.println(authorName);
+        System.out.println(authorLastName);
+        System.out.println(year);
+        System.out.println(isLiked);
+        System.out.println(isRead);
+    }
+}
