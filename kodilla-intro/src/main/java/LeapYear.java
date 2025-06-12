d33c87e9c926e0fadcb50b884d5fb6e3128f2635
Dorota Ganczorz <dganczorz@gmail.com>
2025-06-12 22:51:33 +0200

    Create LeapYear class

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,2 +1,20 @@
 public class LeapYear {
+    public static void main(String[] args) {
+    checkLeapYear(2026);
+    }
+    public static void checkLeapYear(int year) {
+        boolean isLeap;
+        if (year%4 == 0) {
+            isLeap = true;
+        } else if (year%100 == 0) {
+          isLeap = true;
+        } else {
+            isLeap = year % 400 == 0;
+        }
+        if (isLeap) {
+            System.out.println("It's a leap year!");
+        } else {
+            System.out.println("It's not a leap year");
+        }
+    }
 }
