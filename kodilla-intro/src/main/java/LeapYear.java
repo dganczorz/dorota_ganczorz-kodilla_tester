26cac5aecc8dfe3472cdade3f4de3e52cc5d9cbc
Dorota Ganczorz <dganczorz@gmail.com>
2025-06-15 21:21:57 +0200

    Update checkLeapYear method

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,20 +1,37 @@
 public class LeapYear {
     public static void main(String[] args) {
-    checkLeapYear(2026);
+        checkLeapYear(2025);
     }
+
+    /** Checks if year is a leap year
+     * Applies Gregorian calendar rules for years >= 1582
+     * Applies Julian calendar rules for years < 1582
+     * **/
     public static void checkLeapYear(int year) {
         boolean isLeap;
-        if (year%4 == 0) {
-            isLeap = true;
-        } else if (year%100 == 0) {
-          isLeap = true;
+        String calendar = "";
+        if (year >= 1582) {
+            calendar = "Gregorian";
+            if (year % 4 == 0 && year % 100 != 0) {
+                isLeap = true;
+            } else if (year % 4 == 0 && year % 400 == 0) {
+                isLeap = true;
+            } else {
+                isLeap = false;
+            }
         } else {
-            isLeap = year % 400 == 0;
+            calendar = "Julian";
+            if (year % 4 == 0) {
+                isLeap = true;
+            } else {
+                isLeap = false;
+            }
         }
         if (isLeap) {
-            System.out.println("It's a leap year!");
+            System.out.printf("%s is a leap year in %s calendar!", year, calendar);
         } else {
-            System.out.println("It's not a leap year");
+            System.out.printf("%s is not a leap year", year);
         }
     }
 }
+
